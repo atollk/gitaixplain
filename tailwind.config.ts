0f4d8b93707b26697c3baa504536d9d36826292b
Andreas Tollk?tter <andreas.tollkoetter@rwth-aachen.de>
2025-02-18 23:52:45 +0100

    tailwind v4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,19 @@
 import type { Config } from "tailwindcss"
-import * as daisyui from "daisyui"
 
 export default {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
+    content: ["./src/**/*.{html,js,svelte,ts}"],
 
-	theme: {
-		mytheme: {
-			primary: "#3b82f6",
-			secondary: "#16a34a",
-			accent: "#4338ca",
-			neutral: "#170c0e",
-			"base-100": "#361810",
-			info: "#a5f3fc",
-			success: "#a3e635",
-			warning: "#fef08a",
-			error: "#f87171",
-		},
-	},
-
-	plugins: [daisyui.default],
-
-	daisyui: {
-		themes: ["coffee"],
-	},
+    theme: {
+        mytheme: {
+            primary: "#3b82f6",
+            secondary: "#16a34a",
+            accent: "#4338ca",
+            neutral: "#170c0e",
+            "base-100": "#361810",
+            info: "#a5f3fc",
+            success: "#a3e635",
+            warning: "#fef08a",
+            error: "#f87171",
+        },
+    },
 } satisfies Config
